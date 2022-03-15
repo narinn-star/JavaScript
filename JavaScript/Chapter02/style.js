7ec52029b650fba0579f95737bd3621bfebfb687
narinn-star <skfls2618@naver.com>
2022-03-15 22:40:09 +0900

    CodeIt_22.03.15

---
+++ JavaScript/Chapter02/style.js
@@ -0,0 +1,7 @@
+// 스타일 다루기
+const today = document.querySelector('#today');
+const tomorrow = document.querySelector('#tomorrow');
+
+// style 프로퍼티
+today.children[0].style.textDecoration = 'line-through'; //text-decoration은 오류
+today.children[0].style.backgroundColor = '#DDDDDD';  //background-color은 오류
