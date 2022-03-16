682bf29e42d78ad4fc31f127d555005ca8f78f74
narinn-star <skfls2618@naver.com>
2022-03-16 23:20:48 +0900

    CodeIt_22.03.16

--- JavaScript/Chapter02/style.js
+++ JavaScript/Chapter02/style.js
@@ -2,6 +2,20 @@
 const today = document.querySelector('#today');
 const tomorrow = document.querySelector('#tomorrow');
 
+// ele.classList : add, remove, toggle
+// console.log(today.classList);
+// console.log(today.children[1].classList);
+const item = tomorrow.children[1];
+item.classList.add('done'); //('done', 'other') 여러개 추가
+item.classList.remove('done');  //('done', 'other') 여러개 제거
+item.classList.toggle('done') //있으면 제거, 없으면 추가 여러개 토글은 X
+// toggle 두번째 파라미터 역할 : true, false : true는 추가만, false는 제거만
+
+// ele.className
+today.children[1].className = 'done';
+
 // style 프로퍼티
 today.children[0].style.textDecoration = 'line-through'; //text-decoration은 오류
 today.children[0].style.backgroundColor = '#DDDDDD';  //background-color은 오류
+today.children[2].style.textDecoration = 'line-through';
+today.children[2].style.backgroundColor = '#DDDDDD';
