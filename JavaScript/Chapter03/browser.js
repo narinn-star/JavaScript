1353dcda74529c5639743a0972e44f309addff74
narinn-star <skfls2618@naver.com>
2022-03-25 15:18:01 +0900

    CodeIt_22.03.25

---
+++ JavaScript/Chapter03/browser.js
@@ -0,0 +1,24 @@
+// 브라우저의 기본 동작
+const link = document.querySelector('#link');
+const checkbox = document.querySelector('#checkbox');
+const input = document.querySelector('#input');
+const text = document.querySelector('#text');
+
+// event.preventDefault
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  alert('지금은 이동할 수 없습니다.');
+});
+
+input.addEventListener('keydown', function(e) {
+  if(!checkbox.checked){
+    e.preventDefault();
+    alert('체크박스를 먼저 클릭해 주세요.');
+  }
+});
+
+//document.addEventListener('contextmenu', function(e)) => 창 전체에서 우클릭 방지
+text.addEventListener('contextmenu', function(e){ //텍스트 위에서 우클릭 방지
+  e.preventDefault();
+  alert('마우스 오른쪽 클릭은 사용할 수 없습니다.');
+})
