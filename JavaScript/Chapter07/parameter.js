5997b7bbcfe5a2a202ab876acfebd7262e722b9a
narinn-star <skfls2618@naver.com>
2022-04-02 22:54:02 +0900

    CodeIt_22.04.02

---
+++ JavaScript/Chapter07/parameter.js
@@ -0,0 +1,10 @@
+// Parameter
+function greeting(name){
+  console.log(`Hi! My name is ${name}!`);
+}
+
+greeting('JavaScript');
+greeting('Coded it');
+greeting('World');
+
+greeting(); //name == undefined, 파라미터에 기본값 등록할 수 있음 (자바와 같음)
